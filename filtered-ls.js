e9948155a12a079e80f6e86cad4d3395f8c88f16
Jhnqst <johnquisaot@gmail.com>
2025-02-20 16:50:25 +0800

    hooray

---
+++ filtered-ls.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const directoryPath = process.argv[2];
+const extension = '.' + process.argv[3];
+fs.readdir(directoryPath, (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  files.forEach(file => {
+    if (path.extname(file) === extension) {
+      console.log(file);
+    }
+  });
+});
